080e4b573fd9d6b5e1746e01c779ffeba0e37023
beginor <beginor@qq.com>
2017-12-07 10:05:45 +0800

    test client

--- TileServer/wwwroot/index.js
+++ TileServer/wwwroot/index.js
@@ -1,16 +1,25 @@
-﻿(function() {
-    $(document).ready(function () {
-        $.ajax({
-            url: '/rest/account/external-login',
-            success: function (result) {
-                var html = [];
-                result.forEach(function (oauth) {
-                    html.push('<button type="submit" name="Provider" value="' + oauth.AuthenticationType + '">'
-                        + oauth.Caption
-                    + '</button>');
-                });
-                $('#oauth').html(html.join('<br/>'));
-            }
-        });
-    })
-})();
+﻿require([
+    "esri/config",
+    "esri/Map",
+    "esri/views/MapView",
+    "esri/layers/TileLayer",
+    "dojo/domReady!"
+], function(config, Map, MapView, TileLayer) {
+    config.request.corsEnabledServers.push({
+        host: 'localhost:8088',
+        withCredentials: true
+    });
+    var layer = new TileLayer({
+        url: "http://localhost:8088/arcgis/rest/services/BaseMap/AreaMap/MapServer"
+    });
+    // Add layer to map
+    var map = new Map({
+        // basemap: "streets",
+        ground: "world-elevation"
+    });
+    var view = new MapView({
+        container: "map",  // Reference to the DOM node that will contain the view
+        map: map  // References the map object created in step 3
+    });
+    map.layers.add(layer);
+});
