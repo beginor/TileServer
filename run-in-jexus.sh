aea75e90453523e664fa1c496d845de121b846d7
beginor <beginor@qq.com>
2017-12-07 10:31:36 +0800

    update run script

--- run-in-jexus.sh
+++ run-in-jexus.sh
@@ -2,8 +2,9 @@
 docker run --rm \
   --interactive \
   --tty \
-  --volume $(pwd)/OAuthTest:/var/www/default \
-  --volume $(pwd)/default:/usr/jexus/siteconf/default:ro \
+  --volume $(pwd)/TileServer:/var/www/default \
+  --volume $(pwd)/default:/usr/jexus/siteconf/default \
+  --volume /Volumes/25A2/arcgiscache:/arcgiscache \
   --publish 8088:80 \
-  --name jexus-test \
+  --name tile-server \
   beginor/jexus:5.8.2.21
