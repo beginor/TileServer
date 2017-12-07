e32ac66b888b3400227255c57301a929c4281359
beginor <beginor@qq.com>
2017-12-07 13:39:35 +0800

    update client

--- TileServer/wwwroot/index.js
+++ TileServer/wwwroot/index.js
@@ -1,25 +1,29 @@
 ﻿require([
-    "esri/config",
-    "esri/Map",
-    "esri/views/MapView",
-    "esri/layers/TileLayer",
-    "dojo/domReady!"
-], function(config, Map, MapView, TileLayer) {
-    config.request.corsEnabledServers.push({
-        host: 'localhost:8088',
-        withCredentials: true
-    });
+    'dojo/request',
+    'esri/config',
+    'esri/Map',
+    'esri/views/MapView',
+    'esri/layers/TileLayer',
+    'dojo/domReady!'
+], function(request, config, Map, MapView, TileLayer) {
+    // config.request.corsEnabledServers.push({
+    //     host: 'localhost:8088',
+    //     withCredentials: true
+    // });
+    var baseUrl = '/arcgis/rest/services/BaseMap/';
     var layer = new TileLayer({
-        url: "http://localhost:8088/arcgis/rest/services/BaseMap/AreaMap/MapServer"
+        url: '/arcgis/rest/services/BaseMap/AreaMap/MapServer'
     });
     // Add layer to map
     var map = new Map({
-        // basemap: "streets",
-        ground: "world-elevation"
+        // basemap: 'streets',
+        ground: 'world-elevation'
     });
     var view = new MapView({
-        container: "map",  // Reference to the DOM node that will contain the view
-        map: map  // References the map object created in step 3
+        container: 'map',  // Reference to the DOM node that will contain the view
+        map: map,  // References the map object created in step 3
+        center: [113.2, 23.4],
+        zoom: 7
     });
     map.layers.add(layer);
 });
